bdfa32f76a1d55258ac6e3bac7a8f7ae0dc16a17
Boom-Tris <boomtris.jontama@gmail.com>
2025-04-12 01:36:06 +0700

    new

--- src/Nav.ts
+++ src/Nav.ts
@@ -37,7 +37,12 @@ export function Nav() {
         <a href="#skills" class="block py-2 text-gray-600 hover:text-black">Skills</a>
         <a href="#Projects" class="block py-2 text-gray-600 hover:text-black">Project</a>
         <a href="#ContactMe" class="block py-2 text-gray-600 hover:text-black">Contact Me</a>
-        <a href="/cv" class="block py-2 text-gray-600 hover:text-black">CV</a>
+         <a href="/cv" class="bg-black  text-white px-5 rounded-md hover:bg-white hover:text-black hover:font-bold border border-black transition-colors inline-flex items-center  group">
+            CV 
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-4 h-4 ml-3 group-hover:fill-black fill-white transition-colors">
+              <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/>
+            </svg>
+          </a>
       </div>
     </nav>
 
